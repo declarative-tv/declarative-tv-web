3c778536105dd3d2abdd144949938da22bdb67cd
gillchristian <gillchristiang@gmail.com>
2021-01-12 22:56:39 +0100

    feat(api): Add token reset script

---
+++ scripts/reset-token.sh
@@ -0,0 +1,8 @@
+client_id="$1"
+client_secret="$2"
+
+url="https://id.twitch.tv/oauth2/token?client_id=$client_id&client_secret=$client_secret&grant_type=client_credentials"
+
+vercel secrets rm twitch-token
+
+vercel secrets add twitch-token $(curl -XPOST $url \ | jq '.access_token')
